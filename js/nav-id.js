4715d8845209e6ea28cb26674a3c5a93e05e2c6e
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-03 02:09:11 +0600

    post title finished

--- js/nav-id.js
+++ js/nav-id.js
@@ -6,9 +6,32 @@ const loadNav = async category_id => {
 }
 loadNav();
 
-const displayNavId = (data) => {
-    console.log(data.category_id ? data.category_id : 'Do not any id')
-    // for (const id of data) {
-    //     console.log(id.category_id);
-    // }
+const displayNavId = (posts) => {
+    // console.log(posts)
+    const mainContainer = document.getElementById('main-container');
+    const div = document.createElement('div');
+    div.classList.add('row')
+    div.innerHTML = `
+    <div class="col-md-4 my-4">
+        <img src="${posts.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8 my-4">
+        <div class="card-body">
+          <h5 class="card-title">${posts.title}</h5>
+          <p class="card-text">${posts.details}</p>
+          <div class="bottom-part">
+            <div class="author-address float-start">
+                <img class="float-start author-image" src="${posts.image_url}" alt="...">
+                <div class="float-end me-4">
+                    <p class="card-text"><small class="text-muted">${posts.author.name}</small></p>
+                    <p class="card-text"><small class="text-muted">${posts.author.published_date}</small></p>
+                </div>
+                <div class="view-part float-end">
+                    <p class="card-text mt-3"><small class="text-muted">${posts.total_view}</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    mainContainer.appendChild(div);
 }
