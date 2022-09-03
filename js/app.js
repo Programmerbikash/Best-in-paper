43fe4e66f781ef8d3944fdc5323c001d7c9e16e1
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-03 22:15:24 +0600

    first blog complete

--- js/app.js
+++ js/app.js
@@ -28,13 +28,14 @@ const loadNavbarData = async () => {
       }
     });
   };
-  displayNavbarData();
+displayNavbarData();
   
   const loadPostId = (category_id) => {
       const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
       fetch(url)
       .then(res => res.json())
-      .then(data => displayPostData(data.data))
+        .then(data => displayPostData(data.data))
+        .catch(error => console.log(error))
   }
   loadPostId('01');
   
