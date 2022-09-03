050f5dca19247aa9a7e692de32d32d9b31446c65
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-03 20:41:16 +0600

    post half complite

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const loadNavbarData = async () => {
           const ul = document.createElement("ul");
           ul.classList.add("navbar-nav");
           ul.innerHTML = `
-              <li onclick="loadPostId('${singleData.category_id}')" class="nav-item active mx-3 px-1">
+              <li onclick="loadPostId('${singleData.category_id}')" class="nav-item active mx-2 px-1">
                   <a class="nav-link" href="#">${singleData.category_name}</a>
               </li>
         `;
@@ -36,27 +36,70 @@ const loadNavbarData = async () => {
       .then(res => res.json())
       .then(data => displayPostData(data.data))
   }
-  // loadPostId();
+  loadPostId('01');
   
   const displayPostData = allPost => {
       // console.log(allPost);
       const mainContainer = document.getElementById('main-container');
+      mainContainer.innerHTML = ``;
       allPost.forEach(post => {
-          console.log(post);
+        //   console.log(post);
           const div = document.createElement('div');
           div.classList.add('row');
           div.innerHTML = `
-          <div class="col-md-4 border border-1 border-start-0 border-end-0 py-4">
-              <img src="${post.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+          <div class="col-md-3 col-sm-12 border border-1 border-start-0 border-end-0 py-4">
+              <img src="${post.thumbnail_url}" class="img-fluid rounded-start ms-4" alt="...">
         </div>
-        <div class="col-md-8 border border-1 border-start-0 border-end-0 py-4">
+        <div class="col-md-9 col-sm-12 border border-1 border-start-0 border-end-0 py-4">
           <div class="card-body">
               <h5 class="card-title">${post.title}</h5>
-              <p class="card-text">${post.details}</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <p onclick="loadNewsDetails('${post._id}')" class="card-text">${post.details.slice(0, 400)}</p>
+            
+              <!-- Scrollable modal -->
+              
+              <div class="d-flex justify-content-between mt-5">
+                    <div class="d-flex justify-content-between">
+                    <img class="author-image author-image rounded-3 mt-1" src="${post.image_url}">
+                    <div class="ms-2 mt-2">
+                        <h5>${post.author.name ? post.author.name : 'No Author Name'}</h5>
+                        <p>${post.author.published_date}</p>
+                    </div>
+                    </div>
+                    <div class="mt-4">
+                    <h3 class="card-text"><i class="fas fa-eye"></i> ${post.total_view ? post.total_view : 'No View'}</h3>
+                    </div>
+                    <div class="mt-4 d-flex justify-content-between">
+                        <h4><i class="fas fa-star"></i></h4>
+                        <h4><i class="fas fa-star-half-alt"></i></h4>
+                        <h4><i class="far fa-star"></i></h4>
+                    </div>
+                    <div class="mt-4">
+                        <h3><a href="#"><i class="fas fa-arrow-right"></i></a></h3>
+                    </div>
+              </div>
           </div>
          </div>
           `
           mainContainer.appendChild(div);
       });
-  }
+}
+
+const loadNewsDetails = (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data));
+};
+
+const displayNewsDetails = postDetails => {
+    // console.log(postDetails);
+    const details = document.getElementById('modal');
+    details.innerText = ``;
+    for (const detail of postDetails) {
+        // console.log(detail);
+        const p = document.createElement('p');
+        p.innerText = `${detail.details.slice(0, 1500)}`
+        details.appendChild(p);
+    }
+}
+loadNewsDetails();
