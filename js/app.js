2f1910d2ad3fb52699b97f32d01860baa3f9e85b
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-03 16:44:30 +0600

    post start

--- js/app.js
+++ js/app.js
@@ -1,66 +1,62 @@
-const loadInfo = async() => {
-    const url = `https://openapi.programming-hero.com/api/news/categories`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayData(data.data.news_category)
-}
-
-loadInfo();
-
-const displayData = (posts) => {
-    // console.log(posts)
-    for (const post of posts) {
-        const navContainer = document.getElementById('nav-container');
-        // console.log(post);
-        const div = document.createElement('div');
-        div.classList.add('navbar-content')
-        div.innerHTML = `
-            <a onclick="loadNav('${post?.category_id}')" class="nav-link" href="#">${post.category_name}</a>
-        `
-        navContainer.appendChild(div);
-    }
-}
-
-document.getElementById('home-btn').addEventListener('click', function () {
-    loadInfo();
-});
-
-const loadNav = async(category_id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayNavId(data.data[0]);
-}
-
-loadNav();
-
-const displayNavId = (posts) => {
-    console.log(posts)
-    const mainContainer = document.getElementById('main-container');
-    mainContainer.textContent = '';
-    const div = document.createElement('div');
-    div.classList.add('row')
-    div.innerHTML = `
-    <div class="col-md-4 my-4">
-        <img src="${posts.thumbnail_url}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8 my-4">
-        <div class="card-body">
-          <h5 class="card-title">${posts.title}</h5>
-          <p class="card-text">${posts.details}</p>
-          <div class="bottom-part">
-            <div class="author-address float-start">
-                <img class="float-start author-image" src="${posts.image_url}" alt="...">
-                <div class="float-end me-4">
-                    <p class="card-text"><small class="text-muted">${posts.author.name}</small></p>
-                    <p class="card-text"><small class="text-muted">${posts.author.published_date}</small></p>
-                </div>
-                <div class="view-part float-end">
-                    <p class="card-text mt-3"><small class="text-muted">${posts.total_view}</small></p>
-                </div>
-            </div>
+const loadNavbarData = async () => {
+    const response = await fetch(
+      "https://openapi.programming-hero.com/api/news/categories"
+    );
+    const data = await response.json();
+    return data;
+  };
+  
+  const displayNavbarData = async () => {
+    // console.log(loadNavbarData());
+    const data = await loadNavbarData();
+    // console.log(data.data.news_category)
+    const allData = data.data.news_category;
+      const navContainer = document.getElementById("nav-container");
+      const uniqueTitleName = [];
+    allData.forEach((singleData) => {
+      if (uniqueTitleName.indexOf(singleData.category_name) === -1) {
+          // console.log(singleData.category_id);
+          // console.log(singleData.category_name);
+          const ul = document.createElement("ul");
+          ul.classList.add("navbar-nav");
+          ul.innerHTML = `
+              <li onclick="loadPostId('${singleData.category_id}')" class="nav-item active mx-3 px-1">
+                  <a class="nav-link" href="#">${singleData.category_name}</a>
+              </li>
+        `;
+        navContainer.appendChild(ul);
+      }
+    });
+  };
+  displayNavbarData();
+  
+  const loadPostId = (category_id) => {
+      const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+      fetch(url)
+      .then(res => res.json())
+      .then(data => displayPostData(data.data))
+  }
+  // loadPostId();
+  
+  const displayPostData = allPost => {
+      // console.log(allPost);
+      const mainContainer = document.getElementById('main-container');
+      allPost.forEach(post => {
+          console.log(post);
+          const div = document.createElement('div');
+          div.classList.add('row');
+          div.innerHTML = `
+          <div class="col-md-4 border border-1 border-start-0 border-end-0 py-4">
+              <img src="${post.thumbnail_url}" class="img-fluid rounded-start" alt="...">
         </div>
-    </div>
-    `
-    mainContainer.appendChild(div);
-}
+        <div class="col-md-8 border border-1 border-start-0 border-end-0 py-4">
+          <div class="card-body">
+              <h5 class="card-title">${post.title}</h5>
+              <p class="card-text">${post.details}</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+         </div>
+          `
+          mainContainer.appendChild(div);
+      });
+  }
